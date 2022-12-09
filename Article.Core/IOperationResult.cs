c66d409d9cc9f7eca9c0d9b3e1b93ec9ac44e8bb
Bryant <bryant-m@hotmail.com>
2022-12-08 22:03:25 -0400

    Article.Core Added

---
+++ Article.Core/IOperationResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Article.Core
+{
+    public interface IOperationResult
+    {
+        bool IsSuccess { get; set; }
+        string OperationMessage { get; set; }
+
+    }
+
+    public class OperationResult : IOperationResult
+    {
+        public bool IsSuccess { get; set; }
+        public string OperationMessage { get; set; }
+
+        public OperationResult(bool isSuccess, string operationMessage = "")
+        {
+            IsSuccess = isSuccess;
+            OperationMessage = operationMessage;
+        }
+        public OperationResult()
+        {
+
+        }
+    }
+}
