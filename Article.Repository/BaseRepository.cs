2846c2a756edf8f193a9ea27c7427ce4b9b38d4f
CarlosMota18 <Carlosdmota@gmail.com>
2022-12-08 23:56:48 -0400

    Crear proyecto repository con el ArticleRepository con los metodos:
    GetAll
    GetById
    Add
    Update
    Remove
    Commit

---
+++ Article.Repository/BaseRepository.cs
@@ -0,0 +1,58 @@
+﻿using Article.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Article.Repository
+{
+    public interface IArticleRepository
+    {
+        Model.Entities.Article Get(int id);
+        IQueryable<Model.Entities.Article> GetAll();
+        void Add(Model.Entities.Article entity);
+        void Update(Model.Entities.Article entity);
+        void Delete(int id);
+        bool Commit();
+    }
+    public class ArticleRepository : IArticleRepository
+    {
+        private readonly DbContext _dbContext;
+        protected readonly DbSet<Model.Entities.Article> _dbSet;
+        public ArticleRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<Model.Entities.Article>();
+        }
+        public void Add(Model.Entities.Article entity)
+        {
+            _dbSet.Add(entity);
+            Commit();
+        }
+        public bool Commit()
+        {
+            var result = _dbContext.SaveChanges() == 1;
+            return result;
+        }
+        public void Delete(int id)
+        {
+            var item = Get(id);
+            item.IsDeleted = true;
+            Update(item);
+        }
+        public Model.Entities.Article Get(int id)
+        {
+            return _dbSet.Where(x => x.Id == id).FirstOrDefault();
+        }
+        public IQueryable<Model.Entities.Article> GetAll()
+        {
+            return _dbSet.Where(x => x.IsDeleted == false);
+        }
+        public void Update(Model.Entities.Article entity)
+        {
+            _dbSet.Attach(entity);
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            Commit();
+        }
+    }
+}
